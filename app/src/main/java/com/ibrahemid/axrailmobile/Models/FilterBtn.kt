3b292ed1ac11516ae7b0d55a08cd9f158ea2da57
ibrahem <ibrahemid>
2019-05-28 09:37:43 +0800

    first commit

---
+++ app/src/main/java/com/ibrahemid/axrailmobile/Models/FilterBtn.kt
@@ -0,0 +1,12 @@
+package com.ibrahemid.axrailmobile.Models
+
+class FilterBtn (val title: String, var status:Boolean){
+
+//    var status: String = "defaultValue"
+//        get() = field
+//        set(value) { field = value }
+
+}
+
+
+
